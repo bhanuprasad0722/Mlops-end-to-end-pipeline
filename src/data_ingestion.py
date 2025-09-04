aa37b280a73cc2658d11b186539eee03e63cebb2
bhanuprasad0722 <bhanuprasadchellapuram>
2025-09-05 00:03:34 +0530

    done with data_ingestion pipeline

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -22,7 +22,7 @@ file_handler = logging.FileHandler(log_file_path)
 file_handler.setLevel("DEBUG")
 
 # creating a formatter 
-formatter = logging.Formatter("%(asctimme)s - %(name)s - %(levelname)s - %(message)s")
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
 # setting the formatter to both console and file handler's
 console_handler.setFormatter(formatter)
@@ -31,6 +31,59 @@ file_handler.setFormatter(formatter)
 # adding handlers to logger
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
+
+def load_data(data_url):
+    """ takes data url as an input and loads data into a df """
+    try:
+        df = pd.read_csv(data_url)
+        logger.debug("Data loaded from %s",data_url)
+        return df
+    except pd.errors.ParserError as e:
+        logger.error("Failed to parse the file: %s",e)
+        raise
+    except Exception as e:
+        logger.error("Unexpected error occured: %s",e)
+        raise
+
+def preprocess_data(df):
+    """ Basic preprocessing """
+    try:
+        df.drop(columns = ['Unnamed: 2','Unnamed: 3','Unnamed: 4'],inplace = True)
+        df.rename(columns = {'v1':'target','v2':'text'},inplace = True)
+        logger.debug("Data preprocessing Completed")
+        return df
+    except KeyError as e:
+        logger.error("missing column in the dataframe %s",e)
+        raise
+    except Exception as e:
+        logger.error("Unknow error occured while preprocessing the data %s",e)
+        raise
+
+def save_data(test_data,train_data,data_path):
+    """ Saving the data """
+    try:
+        raw_data_path = os.path.join(data_path,'raw')
+        os.makedirs(raw_data_path,exist_ok=True)
+        test_data.to_csv(os.path.join(raw_data_path,'test_data.csv'),index = False)
+        train_data.to_csv(os.path.join(raw_data_path,'train_data.csv'),index = False)
+        logger.debug("Train and Test data saved to path %s",raw_data_path)
+    except Exception as e:
+        logger.error("Unexpected Error Occured %s",e)
+        raise
+
+def main():
+    try:
+        data_url = 'https://raw.githubusercontent.com/bhanuprasad0722/Mlops-end-to-end-pipeline/refs/heads/main/experiments/spam.csv'
+        df = load_data(data_url)
+        processed_df = preprocess_data(df)
+        train_data,test_data = train_test_split(processed_df,test_size=20,random_state=42)
+        save_data(train_data,test_data,data_path = './data')
+    except Exception as e:
+        logger.error("Failed to complete data ingestion process %s",e)
+        raise
+
+if __name__ == '__main__':
+    main()
 
 
 
